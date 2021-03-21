79350791ff0329bb15d3f7f1044e712164be82cd
Daniel <22248414+dartuso@users.noreply.github.com>
2021-03-20 19:33:05 -0600

    Added a Post class for shared posts

---
+++ Pages/Post.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Crowd_CTRL.Pages
+{
+
+    public class Post
+    {
+
+        private Post()
+        {
+            
+            ProfileUrl = "";
+            Username = "";
+            Embed = "";
+            Text = "";
+            Reaction = 0;
+        }
+
+        public Post(string profileUrl, string username, string embed, string text, DateTime time)
+        {
+            ProfileUrl = profileUrl;
+            Username = username;
+            Embed = embed;
+            Text = text;
+            Reaction = 0;
+            PostedDate = time;
+        }
+
+        public Post(string username, string text)
+        {
+            Username = username;
+            Text = text;
+            ProfileUrl = "Icons\\account.svg";
+            Embed = "";
+            Reaction = 0;
+            PostedDate = DateTime.Now;
+        }
+
+        public Post(string text)
+        {
+            Username = "GhostCluster";
+            Text = text;
+            ProfileUrl = "Icons\\account.svg";
+            Embed = "";
+            Reaction = 0;
+            PostedDate = DateTime.Now;
+        }
+        public Post(string username, string text, string embed)
+        {
+            Username = username;
+            Text = text;
+            ProfileUrl = "Icons\\account.svg";
+            Embed = embed;
+            Reaction = 0;
+            PostedDate = DateTime.Now;
+        }
+
+        public DateTime PostedDate { get; set; }
+        public string ProfileUrl { get; set; }
+        public string Username { get; set; }
+        public string Embed { get; set; }
+        public string Text { get; set; }
+        public uint Reaction { get; set; }
+        
+    }
+}
