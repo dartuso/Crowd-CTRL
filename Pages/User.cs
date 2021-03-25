798ba06e1a1ea92955060b665b0361314907010d
Daniel <22248414+dartuso@users.noreply.github.com>
2021-03-24 20:59:16 -0600

    added user class

---
+++ Pages/User.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+
+namespace Crowd_CTRL.Pages
+{
+    public class User
+    {
+        private const string DefaultProfile = "Icons\\account.svg";
+
+        public string ProfilePicture { get; set; }
+        public string Username { get; set; }
+        public string Bio { get; set; }
+        public List<string> Platforms { get; set; }
+        public List<string> Games { get; set; }
+
+        public User()
+        {
+            ProfilePicture = DefaultProfile;
+            Username = "TestUsers";
+            Bio = "";
+            Platforms = new List<string>();
+            Games = new List<string>();
+        }
+
+        public User(string username)
+        {
+            Username = username;
+            ProfilePicture = DefaultProfile;
+        }
+
+        public User(string profilePicture, string username, string bio, List<string> platforms, List<string> games)
+        {
+            ProfilePicture = profilePicture ?? DefaultProfile;
+            Username = username;
+            Bio = bio;
+            Platforms = platforms;
+            Games = games;
+        }
+    }
+}
