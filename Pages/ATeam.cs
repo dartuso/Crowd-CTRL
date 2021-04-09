e498a6f70cc38b1ffe32de6963cb816c36fc518e
tebeica.teodor1 <tebeica.teodor1@cs.ucalgary.ca>
2021-04-09 11:06:08 -0600

    help

--- Pages/ATeam.cs
+++ Pages/ATeam.cs
@@ -8,20 +8,25 @@ namespace Crowd_CTRL.Pages
         public ATeam() {
             teamNo = 0;
             teamName = "";
+            teamClick = "click" + teamNo;
         }
 
         public ATeam(int num, string nam) {
             teamNo = num;
             teamName = nam;
+            teamClick = "click" + teamNo;
         }
         public ATeam(int num) {
             teamNo = num;
             teamName = "";
+            teamClick = "click" + teamNo;
         }
 
-        public int teamNo {get; set; }
+        public int teamNo { get; set; }
         
         [Required]
-        public string teamName {get; set; }
+        public string teamName { get; set; }
+
+        public string teamClick { get; set; }
     }
 }
